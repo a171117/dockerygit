0a393d614e50705d9eaf99e6311f830c15aa1ebe
Jesus Armando Mendoza Romero <a171117@uach.mx>
2022-06-07 14:24:27 -0600

    Primer commit

---
+++ src/main/java/com/fierabras/dockerjavaconsola/DockerJavaConsola.java
@@ -0,0 +1,14 @@
+
+package com.fierabras.dockerjavaconsola;
+
+/**
+ *
+ * @author Jesus Armando Mendoza Romero
+ * a171117 Universidad Autónoma de Chihuahua
+ */
+public class DockerJavaConsola {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo de Docker y Git");
+    }
+}
